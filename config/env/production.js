3ec8102d883e2f1a179d825d472a378c41d50472
William H Durrant <William.H.Durrant@aexp.com>
2016-03-19 20:31:52 +0000

    pulled in from my github repo

--- config/env/production.js
+++ config/env/production.js
@@ -11,6 +11,12 @@ var	db_connection_string = process.env.OPENSHIFT_EXTMONGODB_DB_USERNAME + ":" +
 	process.env.OPENSHIFT_EXTMONGODB_DB_PORT + '/' +
 	process.env.OPENSHIFT_EXTAPP_NAME;
 
+console.log('+========== Start: Prod properties ================+')
+console.log('+== port : ' + port)
+console.log('+== server_ip_address : ' + server_ip_address)
+console.log('+== db : ' + db_connection_string)
+console.log('+== secret : ' + process.env.TODO_SECRET)
+console.log('+========== End: Prod properties ================+')
 
 module.exports = {
 	port: port,
