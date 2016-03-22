9c68977a78c5345b85bef228b94f97ab73465ad0
William H Durrant <William.H.Durrant@aexp.com>
2016-03-22 20:18:14 +0000

    update bugfix

--- api/controllers/todo.controller.js
+++ api/controllers/todo.controller.js
@@ -7,200 +7,232 @@ var mongoose = require('mongoose'),
 
 
 module.exports = {
-  hello: hello,
-  findTodo: findTodo,
-  listTodos: listTodos,
-  createTodo: createTodo,
-  updateTodo: updateTodo,
-  deleteTodo: deleteTodo
+    hello: hello,
+    findTodo: findTodo,
+    listTodos: listTodos,
+    createTodo: createTodo,
+    updateTodo: updateTodo,
+    deleteTodo: deleteTodo
 };
 
 function hello(req, res) {
-  // variables defined in the Swagger document can be referenced using req.swagger.params.{parameter_name}
-  var name = req.swagger.params.name.value || 'stranger';
-  var hello = util.format('Hello, %s!', name);
-  // this sends back a JSON response which is a single string
-  console.log('HELLO ' + JSON.stringify(hello, null , 2));
+    // variables defined in the Swagger document can be referenced using req.swagger.params.{parameter_name}
+    var name = req.swagger.params.name.value || 'stranger';
+    var hello = util.format('Hello, %s!', name);
+    // this sends back a JSON response which is a single string
+    console.log('HELLO ' + JSON.stringify(hello, null, 2));
 
-  res.json(hello);
+    res.json(hello);
 }
 
 function listTodos(req, res) {
-  helper.validateToken(req, res);
-  var queryObj = {};
-  var creator = req.swagger.params.creator.value;
-  console.log('listTodos called... ' + creator);
-  if (creator != undefined){
-    queryObj.creator = creator;
-  } else {
-    return res.status(400).send({
-      message: 'missing creator id for retrieving list of todos'
+    helper.validateToken(req, res);
+    var queryObj = {};
+    var creator = req.swagger.params.creator.value;
+    console.log('listTodos called... ' + creator);
+    if (creator != undefined) {
+        queryObj.creator = creator;
+    } else {
+        return res.status(400).send({
+            message: 'missing creator id for retrieving list of todos'
+        });
+    }
+
+    //for Tag base searches
+    if (req.swagger.params.tag != undefined && req.swagger.params.tag.value) {
+        var tags = req.swagger.params.tag.value;
+        console.log('Weve got tags... : ' + JSON.stringify(tags));
+
+        //check if we're dealing with an array
+        if (Object.prototype.toString.call(tags) === '[object Array]') { //eg. tag=Shopping&tag=Home
+            queryObj.tag = {$in: tags};
+            console.log('Weve got array of tags... : ' + JSON.stringify(queryObj.tag))
+        } else if (tags.split(',').length > 0) { //eg. tag=Shopping%2CHome
+            queryObj.tag = {$in: tags.split(',')};
+            console.log('Weve got array of tags... : ' + JSON.stringify(queryObj.tag))
+        } else {
+            queryObj.tag = {$regex: new RegExp(tags, "i")};
+        }
+    }
+
+    console.log('listTodos called queryObj... ' + JSON.stringify(queryObj));
+
+    Todo.find(queryObj).sort({'priority.priority': -1}).populate('creator', 'name username').exec(function (err, todos) {
+        if (err) {
+            console.log('err listTodos called... ' + err);
+
+            return res.status(400).send({
+                message: helper.getErrorMessage(err)
+            });
+        } else {
+            console.log('Got some todos');
+            res.json(todos);
+        }
     });
-  }
-
-  //for Tag base searches
-  if (req.swagger.params.tag != undefined && req.swagger.params.tag.value){
-    var tags = req.swagger.params.tag.value;
-    console.log('Weve got tags... : ' + JSON.stringify(tags));
-
-    //check if we're dealing with an array
-    if( Object.prototype.toString.call( tags ) === '[object Array]' ) { //eg. tag=Shopping&tag=Home
-      queryObj.tag = {$in: tags};
-      console.log('Weve got array of tags... : ' + JSON.stringify(queryObj.tag))
-    } else if (tags.split(',').length > 0) { //eg. tag=Shopping%2CHome
-      queryObj.tag = {$in: tags.split(',')};
-      console.log('Weve got array of tags... : ' + JSON.stringify(queryObj.tag))
-    } else {
-      queryObj.tag = { $regex : new RegExp(tags, "i") };
-    }
-  }
-
-  console.log('listTodos called queryObj... ' + JSON.stringify(queryObj));
-
-  Todo.find(queryObj).sort({'priority.priority': -1}).populate('creator', 'name username').exec(function(err, todos) {
-    if (err) {
-      console.log('err listTodos called... ' + err);
-
-      return res.status(400).send({
-        message: helper.getErrorMessage(err)
-      });
-    } else {
-      console.log('Got some todos');
-      res.json(todos);
-    }
-  });
 
 }
 
 function findTodo(req, res) {
-  console.log('findTodo called...');
-  helper.validateToken(req, res);
-  var id = req.swagger.params.id.value;
-  Todo.findById(id).populate('creator', 'name username').exec(function(err, retrievedTodo) {
-    if (err) {
-      return res.status(400).send({
-        message: helper.getErrorMessage(err)
-      });
-    } else {
-      res.json(retrievedTodo);
-    }
-  });
+    console.log('findTodo called...');
+    helper.validateToken(req, res);
+    var id = req.swagger.params.id.value;
+    Todo.findById(id).populate('creator', 'name username').exec(function (err, retrievedTodo) {
+        if (err) {
+            return res.status(400).send({
+                message: helper.getErrorMessage(err)
+            });
+        } else {
+            res.json(retrievedTodo);
+        }
+    });
 
 }
 
 function createTodo(req, res) {
-  helper.validateToken(req, res);
-  var todo = new Todo(req.swagger.params.body.value);
-  console.log('save : ' + todo.title + ' by creator ' + todo.creator);
-  if(todo.creator === undefined){
-    return res.status(400).send({
-      message: 'No creator found for new Todo'
-    });
-  }
-  todo.save(function(err, persistedTodo) {
-    if (err) {
-      return res.status(400).send({
-        message: helper.getErrorMessage(err)
-      });
-    } else {
-
-      //Should be a way to populate a object reference without doing another query but for the swagger validation
-      //the creator needs populating
-      Todo.findById(persistedTodo._id).populate('creator', 'name username').exec(function(err, retrievedTodo) {
-        if (err) {
-          return res.status(400).send({
-            message: helper.getErrorMessage(err)
-          });
-        } else {
-          res.json(retrievedTodo);
-        }
-      });
+    helper.validateToken(req, res);
+    var todo = new Todo(req.swagger.params.body.value);
+    console.log('save : ' + todo.title + ' by creator ' + todo.creator);
+    if (todo.creator === undefined) {
+        return res.status(400).send({
+            message: 'No creator found for new Todo'
+        });
     }
-  });
+    todo.save(function (err, persistedTodo) {
+        if (err) {
+            return res.status(400).send({
+                message: helper.getErrorMessage(err)
+            });
+        } else {
+
+            //Should be a way to populate a object reference without doing another query but for the swagger validation
+            //the creator needs populating
+            Todo.findById(persistedTodo._id).populate('creator', 'name username').exec(function (err, retrievedTodo) {
+                if (err) {
+                    return res.status(400).send({
+                        message: helper.getErrorMessage(err)
+                    });
+                } else {
+                    res.json(retrievedTodo);
+                }
+            });
+        }
+    });
 
 
 }
 
+
 function updateTodo(req, res) {
-  var todo = new Todo(req.swagger.params.body.value);
-  console.log('updateTodo called with todo : ' + JSON.stringify(todo, null, 2))
-  //todo.title = req.body.title;
-  //todo.tag = req.body.tag;
-  //todo.comment = req.body.comment;
-  //todo.completed = req.body.completed;
-  //todo.status = req.body.status;
-  //todo.priority = req.body.priority;
-  delete todo._id;
-  todo.isNew = false;
-  todo.save(function(err, updatedTodo) {
-    if (err) {
-      console.log('Got err : ' + err)
-      return res.status(400).send({
-        message: helper.getErrorMessage(err)
-      });
-    } else {
+    var todo = new Todo(req.swagger.params.body.value);
+    console.log('updateTodo called with todo : ' + JSON.stringify(todo, null, 2))
 
-      console.log('Updated todo : ' + JSON.stringify(updatedTodo, null, 2));
-      //res.json(updatedTodo);
+    var query = {'_id': todo._id};
 
-      //Should be a way to populate a object reference without doing another query but for the swagger validation
-      //the creator needs populating
-      Todo.findById(updatedTodo._id).populate('creator', 'name username').exec(function(err, retrievedTodo) {
+    Todo.findOneAndUpdate(query, todo, {upsert: true}, function (err, updatedTodo) {
         if (err) {
-          return res.status(400).send({
-            message: helper.getErrorMessage(err)
-          });
+            console.log('Got err : ' + err)
+            return res.status(400).send({
+                message: helper.getErrorMessage(err)
+            });
         } else {
-          console.log('Updated todo : ' + JSON.stringify(retrievedTodo, null, 2));
-          res.json(retrievedTodo);
-        }
-      });
 
-    }
-  });
+            console.log('Updated todo : ' + JSON.stringify(updatedTodo, null, 2));
+            //res.json(updatedTodo);
+            Todo.findById(updatedTodo._id).populate('creator', 'name username').exec(function (err, retrievedTodo) {
+                if (err) {
+                    return res.status(400).send({
+                        message: helper.getErrorMessage(err)
+                    });
+                } else {
+                    console.log('Updated todo : ' + JSON.stringify(retrievedTodo, null, 2));
+                    res.json(retrievedTodo);
+                }
+            });
+        }
+    });
+
+}
+
+function __updateTodo(req, res) {
+    var todo = new Todo(req.swagger.params.body.value);
+    console.log('updateTodo called with todo : ' + JSON.stringify(todo, null, 2))
+    //todo.title = req.body.title;
+    //todo.tag = req.body.tag;
+    //todo.comment = req.body.comment;
+    //todo.completed = req.body.completed;
+    //todo.status = req.body.status;
+    //todo.priority = req.body.priority;
+    delete todo._id;
+    todo.isNew = false;
+    todo.save(function (err, updatedTodo) {
+        if (err) {
+            console.log('Got err : ' + err)
+            return res.status(400).send({
+                message: helper.getErrorMessage(err)
+            });
+        } else {
+
+            console.log('Updated todo : ' + JSON.stringify(updatedTodo, null, 2));
+            //res.json(updatedTodo);
+
+            //Should be a way to populate a object reference without doing another query but for the swagger validation
+            //the creator needs populating
+            Todo.findById(updatedTodo._id).populate('creator', 'name username').exec(function (err, retrievedTodo) {
+                if (err) {
+                    return res.status(400).send({
+                        message: helper.getErrorMessage(err)
+                    });
+                } else {
+                    console.log('Updated todo : ' + JSON.stringify(retrievedTodo, null, 2));
+                    res.json(retrievedTodo);
+                }
+            });
+
+        }
+    });
 };
 
 
 function _updateTodo(req, res) {
-  helper.validateToken(req, res);
-  var todo = new Todo(req.swagger.params.body.value);
-  //console.log('updateTodo called with updated Todo : ' + JSON.stringify(todo, null, 2));
-  todo.isNew = false;
-  todo.save(function(err, updatedTodo) {
-    if (err) {
-      console.log('err got ' + err);
-      return res.status(400).send({
-        message: helper.getErrorMessage(err)
-      });
-    } else {
-      //Should be a way to populate a object reference without doing another query but for the swagger validation
-      //the creator needs populating
-      Todo.findById(updatedTodo._id).populate('creator', 'name username').exec(function(err, retrievedTodo) {
+    helper.validateToken(req, res);
+    var todo = new Todo(req.swagger.params.body.value);
+    //console.log('updateTodo called with updated Todo : ' + JSON.stringify(todo, null, 2));
+    todo.isNew = false;
+    todo.save(function (err, updatedTodo) {
         if (err) {
-          return res.status(400).send({
-            message: helper.getErrorMessage(err)
-          });
+            console.log('err got ' + err);
+            return res.status(400).send({
+                message: helper.getErrorMessage(err)
+            });
         } else {
-          res.json(retrievedTodo);
+            //Should be a way to populate a object reference without doing another query but for the swagger validation
+            //the creator needs populating
+            Todo.findById(updatedTodo._id).populate('creator', 'name username').exec(function (err, retrievedTodo) {
+                if (err) {
+                    return res.status(400).send({
+                        message: helper.getErrorMessage(err)
+                    });
+                } else {
+                    res.json(retrievedTodo);
+                }
+            });
         }
-      });
-    }
-  });
+    });
 }
 
 function deleteTodo(req, res) {
-  helper.validateToken(req, res);
-  var id = req.swagger.params.id.value;
-  console.log('deleteTodo called for id ' + id);
-  Todo.findByIdAndRemove(id, function(err) {
-    if (err) {
-      return res.status(400).send({
-        message: helper.getErrorMessage(err)
-      });
-    } else {
-      var msg = {"message":"Todo successfully deleted"};
-      // this sends back a JSON response which is a single string
-      res.json(msg);
-    }
-  });
+    helper.validateToken(req, res);
+    var id = req.swagger.params.id.value;
+    console.log('deleteTodo called for id ' + id);
+    Todo.findByIdAndRemove(id, function (err) {
+        if (err) {
+            return res.status(400).send({
+                message: helper.getErrorMessage(err)
+            });
+        } else {
+            var msg = {"message": "Todo successfully deleted"};
+            // this sends back a JSON response which is a single string
+            res.json(msg);
+        }
+    });
 };
